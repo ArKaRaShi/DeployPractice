05da7648cfe6fd21e3422fb7a3aea3ad27b7fed0
ArKaRaShi <rnwz12345@gmail.com>
2024-09-14 22:00:45 +0700

    Init

---
+++ main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+import nest_asyncio
+import uvicorn
+
+app = FastAPI()
+
+@app.get("/")
+async def main():
+    return {"Lucky": "Hello"}
+
+nest_asyncio.apply()
+uvicorn.run(app, host="0.0.0.0")
